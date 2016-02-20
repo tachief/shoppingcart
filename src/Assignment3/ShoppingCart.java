7b9ed1a3c63fbb4e39e6b0220b83b6caa5993910
jpolarinakis <jpolarinakis@utexas.edu>
2016-02-20 13:42:10 -0600

    Added files via upload

--- src/Assignment3/ShoppingCart.java
+++ src/Assignment3/ShoppingCart.java
@@ -3,8 +3,16 @@ package Assignment3;
 import java.util.ArrayList;
 
 public class ShoppingCart {
-	private ArrayList<Item> cart = new ArrayList<Item>();
 	
+	private ArrayList<Item> cart = new ArrayList<Item>();
+	private final int nPlace = 1;
+	private final int pPlace = 2;
+	private final int qPlace = 3;
+	private final int wPlace = 4;
+	private final int psPlace = 5;
+	private final int fPlace = 6;
+	private final int sPlace = 7;
+	private final int csPlace = 5;
 	public int delete(String data)
 	{
 		int ret = 0;
@@ -18,22 +26,98 @@ public class ShoppingCart {
 		}
 		return ret;
 	}
-	public void print()
+	public void insert(String data[])
 	{
-		System.out.println("The contents of the cart are: ");
+		double price = Double.valueOf(data[pPlace]);
+		int quantity = Integer.valueOf(data[qPlace]);
+		double weight = Double.valueOf(data[wPlace]);
+
+		
+		if(data[nPlace].equals("ELECTRONICS"))
+		{
+			boolean pShipping = Boolean.valueOf(data[psPlace]);
+			boolean fragile = Boolean.valueOf(data[fPlace]);
+			Electronics toAdd = new Electronics(data[nPlace], price,quantity,weight,pShipping,fragile,data[sPlace]);
+			cart.add(toAdd);
+		}
+		if(data[nPlace].equals("CLOTHING"))
+		{
+			Clothing toAdd = new Clothing(data[nPlace],price,quantity,weight,data[csPlace]);
+			cart.add(toAdd);
+			
+		}
+		if(data[nPlace].equals("GROCERY"))
+		{
+			boolean pShipping = Boolean.valueOf(data[psPlace]);
+			boolean perishable = Boolean.valueOf(data[fPlace]);
+			Grocery toAdd = new Grocery(data[nPlace],price,quantity,weight,pShipping,perishable);
+			cart.add(toAdd);
+		}
+	}
+	public void print()
+	{	
+		final int pricePlace = 4;
+		final int quantityPlace = 1;
+		final int weightPlace = 2;
+		final int fpPlace = 3;
+		double totalPrice =0;
+		String ask = " ** ";
+		String tab = "\t";
+		ArrayList<Item> sortedCart = sort(cart);
+		System.out.println("************************************");
+		System.out.println("**The contents of the cart are: **");
 		for(int i =0; i < cart.size();i++)
 		{	
-			String base = "" + i + ") ";
+	
+			String base = ask + "" + i + ") " + sortedCart.get(i).getName() + ask ;
 			String[] data = cart.get(i).getTraits();
 			System.out.println(" ");
 			System.out.println(base);
+			System.out.println(ask + tab + "Price: " + data[pricePlace] + ask );
+			System.out.println(ask + tab + "Quantity: " + data[quantityPlace]+ ask);
+			System.out.println(ask + tab + "Weight: " + data[weightPlace]+ ask);
+
 			if(cart.get(i) instanceof Grocery)
 			{
-				
+			System.out.println(ask + tab + "Premium Shipping: " + data[psPlace] + ask);
+			System.out.println(ask + tab + "Perishable: " + data[fPlace] + ask);
 			}
-			if(cart.get(i) instanceof Clothing){}
-			if(cart.get(i) instanceof Electronics){}
+			if(cart.get(i) instanceof Clothing)
+			{
+			System.out.println(ask + tab + "Shipping to: " + data[csPlace] + ask);
+			}
+			if(cart.get(i) instanceof Electronics)
+			{
+			System.out.println(ask + tab + "Premium Shipping: " + data[psPlace] + ask);
+			System.out.println(ask + tab + "Perishable: " + data[fPlace] + ask);
+			System.out.println(ask + tab + "Perishable: " + data[sPlace] + ask);
+			}
+			System.out.println(ask + tab + "Final Price: " + data[fpPlace]+ ask);
+			totalPrice += Integer.valueOf(data[fpPlace]);
+			System.out.println("Total Price of Shopping Cart is: " + totalPrice);
+			System.out.println("************************************");
 		}
+	}
+	public ArrayList<Item> sort(ArrayList<Item> data)
+	{
+		ArrayList<Item> ret = new ArrayList<Item>();
+		int size = data.size();
+		
+		for(int j = 0; j< size; j++){
+		String next = data.get(j).getName();
+		int nextPlace = 0;
+		for(int i =0; i< data.size();i++)
+		{
+			if(data.get(i).getName().compareTo(next) < 1)
+			{
+				nextPlace = i;
+				next = data.get(i).getName();
+			}
+		}
+		ret.add(data.get(nextPlace));
+		data.remove(data.get(nextPlace));
+		}
+		return ret;
 	}
 
 	
